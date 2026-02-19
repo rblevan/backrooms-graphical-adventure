683fb071391aac14f4cfe36e89e4c97866902703
Stéphane <stephane.castan@outlook.fr>
2026-02-19 22:22:12 +0100

    map redimensionné + restructuration du mvc

--- backrooms/src/main/java/fr/univpoitiers/backrooms/controller/WorldController.java
+++ backrooms/src/main/java/fr/univpoitiers/backrooms/controller/WorldController.java
@@ -3,24 +3,27 @@ package fr.univpoitiers.backrooms.controller;
 import fr.univpoitiers.backrooms.model.entity.Hero;
 import fr.univpoitiers.backrooms.model.enumeration.commands.Commands;
 import fr.univpoitiers.backrooms.model.item.Backpack;
-import fr.univpoitiers.backrooms.model.item.Items;
 import fr.univpoitiers.backrooms.model.world.Locations;
+import fr.univpoitiers.backrooms.model.world.ObstacleMap;
+import fr.univpoitiers.backrooms.model.world.Position;
 import fr.univpoitiers.backrooms.model.world.WorldBuilder;
 import fr.univpoitiers.backrooms.view.WorldWindow;
+import javafx.scene.Scene;
 import javafx.scene.control.TextInputDialog;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
 
-import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 public class WorldController {
     private Hero player;
     private Stage primaryStage;
-    private List<Items> items;
 
     public WorldController(Stage primaryStage) {
         this.primaryStage = primaryStage;
@@ -29,6 +32,8 @@ public class WorldController {
     public void prepareWorld() {
         // 1. Initialisation du monde
         WorldBuilder worldBuilder = new WorldBuilder();
+
+        //a revoir
         String startLevelPath = worldBuilder.getLocations().keySet().stream()
                 .filter(path -> path.contains("Map_empty.png"))
                 .findFirst()
@@ -40,13 +45,18 @@ public class WorldController {
         Backpack backpack = new Backpack("Blue backpack", "A standard backpack", 120);
         String playerDesc = "an ordinary person who has lived a quiet, unremarkable life...";
 
-        this.player = new Hero(playerName, 100, playerName, 20, playerDesc, backpack, startLocation);
+        //l'image du hero
+        String pathImage = Objects.requireNonNull(getClass().getResource("/images/downHero.png")).toExternalForm();
+        Image imageHero = new Image(pathImage);
+        this.player = new Hero(playerName, 100, playerName, 20, playerDesc, backpack, startLocation, imageHero, new Position(100,100));
     }
+
+
 
     public void startWorldMode() {
         prepareWorld();
         Commands commandProcessor = new Commands(player, player.getLocation());
-        WorldWindow worldWindow = new WorldWindow(primaryStage,commandProcessor);
+        WorldWindow worldWindow = new WorldWindow(primaryStage, this,commandProcessor);
     }
 
     private String askPlayerName() {
@@ -64,25 +74,6 @@ public class WorldController {
     }
 
     public Hero getPlayer() {
-        return player;
-    }
-
-    public void addItem(){
-
-    }
-
-    public void createGrid(GridPane grid, int columns, int rows,int cellSize) {
-
-        for (int i = 0; i < columns; i++) {
-            ColumnConstraints col = new ColumnConstraints(cellSize);
-            grid.getColumnConstraints().add(col);
-        }
-
-        for (int i = 0; i < rows; i++) {
-            RowConstraints row = new RowConstraints(cellSize);
-            grid.getRowConstraints().add(row);
-        }
-        grid.setGridLinesVisible(true); // debug
-
+        return this.player;
     }
 }
