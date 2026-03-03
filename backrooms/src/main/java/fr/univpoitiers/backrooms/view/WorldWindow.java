f7c57da625e2218faacf67c5211b06fd54134e76
scastan <scastan@ubuntu.sfasp2mi.univ-poitiers.fr>
2026-03-03 18:11:28 +0100

    fonctionnalité deplacements

--- backrooms/src/main/java/fr/univpoitiers/backrooms/view/WorldWindow.java
+++ backrooms/src/main/java/fr/univpoitiers/backrooms/view/WorldWindow.java
@@ -3,9 +3,12 @@ package fr.univpoitiers.backrooms.view;
 import fr.univpoitiers.backrooms.controller.WorldController;
 import fr.univpoitiers.backrooms.model.enumeration.commands.Commands;
 import fr.univpoitiers.backrooms.model.world.ObstacleMap;
+import javafx.animation.AnimationTimer;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
@@ -17,6 +20,12 @@ public class WorldWindow {
     private final Stage stage;
     private final Commands commands;
     private WorldController worldController;
+
+    private boolean goSouth;
+    private boolean goNorth;
+    private boolean goWest;
+    private boolean goEast;
+    private boolean running;
 
     public WorldWindow(Stage stage, WorldController worldController, Commands commands){
         this.stage = stage;
@@ -47,12 +56,33 @@ public class WorldWindow {
 
         //Ajout hero
         ImageView heroView = addImageToLayout(gameLayer,worldController.getPlayer().getImage());
+        AnimationTimer animationTimer = new AnimationTimer() {
+            @Override
+            public void handle(long l) {
+                double speed = 2;
+                if (running){
+                    speed = speed*2;
+                }
+                if (goNorth){
+                    heroView.setTranslateY(heroView.getTranslateY()-speed);
+                }else if (goEast){
+                    heroView.setTranslateX(heroView.getTranslateX()+speed);
+                }else if (goSouth){
+                    heroView.setTranslateY(heroView.getTranslateY()+speed);
+                }else if (goWest){
+                    heroView.setTranslateX(heroView.getTranslateX()-speed);
+                }
+            }
+        };
 
         // Pour que la map reste centrée si on agrandit la fenêtre
         root.setCenter(gameLayer);
 
         // Scene setup
-        Scene scene = new Scene(root, 800, 600);
+        Scene scene = new Scene(root, 1400, 1400);
+
+        movePlayerKeyBoard(heroView,scene);
+        animationTimer.start();
 
         stage.setTitle("Backrooms game - OpenWorld");
         stage.setScene(scene);
@@ -60,6 +90,58 @@ public class WorldWindow {
 
         //gameLayer.requestFocus();
         ObstacleMap obstacleMap = new ObstacleMap(28,28);
+    }
+
+    public void movePlayerKeyBoard(ImageView hero, Scene scene){
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                switch(keyEvent.getCode()){
+                    case Z:
+                        goNorth = true;
+                        break;
+                    case S:
+                        goSouth = true;
+                        break;
+                    case Q:
+                        goWest = true;
+                        break;
+                    case D:
+                        goEast = true;
+                        break;
+                    case SHIFT:
+                        running = true;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+
+        scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                switch (keyEvent.getCode()){
+                    case Z:
+                        goNorth = false;
+                        break;
+                    case S:
+                        goSouth = false;
+                        break;
+                    case Q:
+                        goWest = false;
+                        break;
+                    case D:
+                        goEast = false;
+                        break;
+                    case SHIFT:
+                        running = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
     }
 
     public ImageView addImageToLayout(Pane pane, Image image) {
